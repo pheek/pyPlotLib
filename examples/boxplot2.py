45b3d8908370071f0637d7009de687b00d5c6a90
phi <phi@freimann.eu>
2026-01-11 16:27:14 +0100

    duplikate gelösch

--- examples/boxplot2.py
+++ examples/boxplot2.py
@@ -16,5 +16,5 @@ daten_klasse_b = [5, 8, 9, 9, 10, 10, 11, 12, 13, 15, 19]
 draw_boxplot(ax, daten_klasse_a, y_position=2, label="Klasse A", color='#3498db', axis_label="Noten")
 draw_boxplot(ax, daten_klasse_b, y_position=4, label="Klasse B", color='#2ecc71')
 
-#save_system(fig, "boxplot1.png")
+#save_system(fig, "boxplot2.png")
 plt.show()
