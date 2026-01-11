4e0e51c99fcab995ca74732903a577429cb6a2ad
phi <phi@freimann.eu>
2026-01-11 17:12:05 +0100

    Neue Zahlen

--- examples/function5_mitPolygon.py
+++ examples/function5_mitPolygon.py
@@ -7,14 +7,14 @@ import matplotlib.pyplot as plt
 fig, ax = bmsw_coordinate_system(-1, +4, -1, +3)
 
 def wurzelFunktion(x):
-	return x**0.5 # wurzel
+	return 1.5 * x**0.5 # wurzel
 
 # B 1. Eine Parabel in Scheitelform
 draw_function_into_system(ax, wurzelFunktion, (0, 3.5), label="Parabel", color='#ccddee')
 
 # B Polygon
 #   Punkte definieren (z.B. ein Parallelogramm)
-xx = 3.5
+xx = 2.75
 dreieck_punkte = [[0,0], [xx, 0], [xx, wurzelFunktion(xx)]]
 
 # C Zeichnen
@@ -23,7 +23,7 @@ draw_polygon(ax, dreieck_punkte, color='#8e44ff', label="Fläche A")
 # D Eckpunkte Beschriften
 labels = ["", "B", "C"]
 for i, p in enumerate(dreieck_punkte):
-    ax.text(p[0] + 0.1, p[1] + 0.05, labels[i], fontweight='bold', ha='center')
+    ax.text(p[0] + 0.12, p[1] + 0.02, labels[i], fontweight='bold', ha='center')
 
 # E Legende anzeigen
 ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
